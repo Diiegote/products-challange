7387c0519794f68587dd6340a43cc3e2ea246130
Diiegote <diego.elprado22@gmail.com>
2024-04-05 15:15:09 -0300

    feat: :sparkles: Routes
    Se crean las rutas de los productos

---
+++ src/routes/router.js
@@ -0,0 +1,19 @@
+const { Router } = require("express");
+const allProductsControllers = require("../controllers/allProductsControllers");
+const getProductByIdControllers = require("../controllers/getProductByIdControllers");
+const editProductControllers = require("../controllers/editProductControllers");
+const createProductControllers = require("../controllers/createProductControllers");
+const isValidObjectId = require("../middlewares/validationId");
+const asyncCatched = require("../utils/asyncCatched");
+const validateCreateProduct = require("../middlewares/validationProducts/validationCreateProduct");
+const validationResultExpress = require("../middlewares/validationResultExpress");
+const validateEditProduct = require("../middlewares/validationProducts/validationEditProduct");
+
+const router = Router();
+
+router.get('/products', asyncCatched(allProductsControllers));
+router.get('/product/:id', isValidObjectId("id"), asyncCatched(getProductByIdControllers));
+router.post('/new-product', validateCreateProduct, validationResultExpress, asyncCatched(createProductControllers));
+router.patch('/edit-product/:id', isValidObjectId("id"), validateEditProduct, validationResultExpress, asyncCatched(editProductControllers));
+
+module.exports = router;
