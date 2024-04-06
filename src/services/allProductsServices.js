d4f00109dd5771112f216fd8c11e46cfa7365853
Diiegote <diego.elprado22@gmail.com>
2024-04-06 11:58:11 -0300

    feat: :sparkles: Paginacion
    Se modifica la paginacion agregando el total de productos y el total de paginas

--- src/services/allProductsServices.js
+++ src/services/allProductsServices.js
@@ -2,12 +2,14 @@ const { Product } = require("../schemas");
 const ClientError = require("../utils/errors");
 
 
-const allProductsServices = async (page, limit,nombre) => {
+const allProductsServices = async (page, limit = 5, nombre) => {
    const products = await Product.find({ nombre: { $regex: new RegExp(nombre, "i") } })
       .skip((page - 1) * limit)
       .limit(limit);
+   const totalProductos = await Product.countDocuments({ nombre: { $regex: new RegExp(nombre, "i") } });
+   const totalPages = Math.ceil(totalProductos / limit);
    if (!products.length) throw new ClientError('No se encontraron productos', 404);
-   return products;
+   return { products, totalProductos, totalPages };
 };
 
 module.exports = allProductsServices;
