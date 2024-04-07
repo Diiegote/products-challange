dee73941c25753347c01fb8046f5f2f7cc0298ec
Diiegote <diego.elprado22@gmail.com>
2024-04-06 22:08:41 -0300

    fix: :bug: fixed bug
    fixed bug

--- src/controllers/allProductsControllers.js
+++ src/controllers/allProductsControllers.js
@@ -3,8 +3,8 @@ const response = require("../utils/response");
 
 
 const allProductsControllers = async ({ query }, res) => {
-   const { page, limit, nombre } = query;
-   const products = await allProductsServices(page, limit, nombre);
+   const { page, limit, nombre, categoria } = query;
+   const products = await allProductsServices(page, limit, nombre, categoria);
    response(res, 201, products);
 };
 
--- src/services/allProductsServices.js
+++ src/services/allProductsServices.js
@@ -2,7 +2,18 @@ const { Product } = require("../schemas");
 const ClientError = require("../utils/errors");
 
 
-const allProductsServices = async (page, limit = 8, nombre) => {
+const allProductsServices = async (page, limit = 8, nombre, categoria) => {
+   if (categoria) {
+      const products = await Product.find({ categoria: { $regex: new RegExp(categoria, "i") } })
+         .skip((page - 1) * limit)
+         .limit(limit);
+      console.log(categoria);
+      const totalProductos = await Product.countDocuments({ categoria: { $regex: new RegExp(categoria, "i") } });
+      console.log(totalProductos);
+      const totalPages = Math.ceil(totalProductos / limit);
+      if (!products.length) throw new ClientError('No se encontro la categoria', 404);
+      return { products, totalPages, totalProductos };
+   }
    const products = await Product.find({ nombre: { $regex: new RegExp(nombre, "i") } })
       .skip((page - 1) * limit)
       .limit(limit);
