45748755eb7f32456fa0af9d6e7537b96bdd4f3a
Diiegote <diego.elprado22@gmail.com>
2024-04-07 21:07:35 -0300

    se modifica el editar product para que no quede en blanco si me envian campos vacios.

--- src/services/editProductServices.js
+++ src/services/editProductServices.js
@@ -3,7 +3,17 @@ const ClientError = require("../utils/errors");
 
 
 const editProductServices = async (id, body) => {
-   const editProduct = await Product.findByIdAndUpdate(id, body, { new: true })
+   const { nombre, precio, total_productos, imagen, descripcion, categoria, disponibilidad } = body;
+   const product = await getProductByIdControllers(id);
+   const editProduct = await Product.findByIdAndUpdate(id, {
+      nombre: nombre ? nombre : product.data.nombre,
+      precio: precio ? precio : product.data.precio,
+      total_productos: total_productos ? total_productos : product.data.total_productos,
+      imagen: imagen ? imagen : product.data.imagen,
+      descripcion: descripcion ? descripcion : product.data.descripcion,
+      categoria: categoria ? categoria : product.data.categoria,
+      disponibilidad: disponibilidad ? disponibilidad : product.data.disponibilidad,
+   }, { new: true })
    if (!editProduct) throw new ClientError('No se encontro el producto', 404);
    return editProduct;
 };
